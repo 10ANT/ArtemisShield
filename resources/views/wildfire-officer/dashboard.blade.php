4718ae13347bd3d17ea648fb23bad0a43622702e
TonyDann <garybryan2021@gmail.com>
2025-06-09 20:06:05 -0500

    about_to_add_fire_hydrants

--- resources/views/wildfire-officer/dashboard.blade.php
+++ resources/views/wildfire-officer/dashboard.blade.php
@@ -4,9 +4,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>ArtemisShield - Wildfire Protection Dashboard</title>
+    
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 
     @include('partials.styles')
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WildfireOfficerController;
 use App\Http\Controllers\ApiController;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('main');
