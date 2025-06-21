97f90a35e8864f20edd74d6bd9ccfc7119cbcd24
Your Name <your.email@example.com>
2025-06-21 01:21:57 -0500

    redeploy

--- resources/views/end-user/dashboard.blade.php
+++ resources/views/end-user/dashboard.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     @include('partials.styles')
     
-    <!-- This loads your CSS and the globally configured Echo instance via Vite -->
+    <!-- This loads your CSS and the globally configured Echo instance via Vites -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <style>
